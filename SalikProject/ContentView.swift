d46096746ecdc3a0e49315a4d5bf86ead04b9d4a
ralrobaili <ralrobaili@gmail.com>
2025-12-07 09:37:14 +0300

    Initial Commit

---
+++ SalikProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SalikProject
+//
+//  Created by raghad alenezi on 16/06/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SalikProject/SalikProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalikProjectApp.swift
+//  SalikProject
+//
+//  Created by raghad alenezi on 16/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct SalikProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
